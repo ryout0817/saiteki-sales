28dcd34e9a7dcf2dfed62d84be6a5d305c5be8c0
Kami-Ryotaro <kamiryo.0817@gmail.com>
2025-10-28 23:43:31 +0900

    Markdown All in One

---
+++ src/components/SearchForm/SearchFormMock.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SearchForm } from '.././SearchForm';
+
+export const SearchFormMock = () => {
+  return (
+    <div style={{ padding: '20px', border: '1px solid #ccc', margin: '20px' }}>
+      <h1>SearchForm Mock Test</h1>
+      <p>Below is the SearchForm component:</p>
+      <SearchForm />
+    </div>
+  );
+};
