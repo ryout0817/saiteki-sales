4646b76e5660c9f8f8e1aa4ac89b9e728c212734
Kami-Ryotaro <kamiryo.0817@gmail.com>
2025-10-29 19:47:54 +0900

    add list file

--- src/features/searchForm/search-form-list.ts
+++
@@ -1,7 +0,0 @@
-export const REMOTE_OPTIONS = [
-  { value: '', label: '選択してください' },
-  { value: 'full_remote', label: 'フルリモート' },
-  { value: 'basic_remote', label: '基本リモート' },
-  { value: 'partial_remote', label: '一部リモート' },
-  { value: 'on_site', label: '常駐' },
-];
